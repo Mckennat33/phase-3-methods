1417e620ae3c86536cb9f3900a77a4d9692b52da
Thomas <thomasmckenna12gmail.com>
2022-08-17 21:52:30 -0500

    commit tree f9792109e62d51a39f5b213cf0729213f804461c

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    "Hello, #{name}!"
+end
+
+greet() 
+
+def greet_with_default name = "programmer"
+    "Hello, #{name}!"
+end
+
+
+def add (num1, num2)
+    num1 + num2
+end
+
+
+
+
+def halve number 
+    if (typeof number !== "number") nil
+    number / 2
+end 
+
